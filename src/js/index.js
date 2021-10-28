b7f9df6fa7026acf98015d6bd1e69e18fc3fad7c
KamilGetler <kamilgetler@gmail.com>
2021-10-28 12:41:33 +0200

    NewFormatCode

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,12 @@
 import '../scss/main.scss';
 
 
-console.log('HELLO 🚀')
+
+
+const button = document.querySelector("img");
+
+button.addEventListener('click',() => {
+  document.querySelector("img").classList.toggle("kropla__aim");
+
+  console.log('simano');
+});
